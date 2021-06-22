a6d4ea2f579306d9ee0b057497abf52c261a8028
Karan Jadhav <karan.jadhavhkr@gmail.com>
2021-06-22 20:42:33 +0530

    Project Creation

---
+++ intensel_backend/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from intensel_backend.users.models import User
+from intensel_backend.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
